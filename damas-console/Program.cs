1c86fccf0a7294974c15d70b7a9c04808b11b01e
Adriano Pimenta <apimenta.usp@gmail.com>
2020-11-03 18:21:30 -0300

    Implementando metodos para colocar e retirar pecas do tabuleiro

--- damas-console/Program.cs
+++ damas-console/Program.cs
@@ -6,14 +6,20 @@ namespace damas_console {
     class Program {
         static void Main(string[] args) {
 
-            Tela.imprimirTelaInicial();
+            try {
+                Tela.imprimirTelaInicial();
 
-            Console.Clear();
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            Tela.imprimirTabuleiro(tab);
+                Console.Clear();
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                Tela.imprimirTabuleiro(tab);
 
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey(true);
         }
     }
 }
